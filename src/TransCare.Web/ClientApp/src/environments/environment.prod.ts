77c6a5f3476afaf8654d19a36f27aed7acc2fa0c
Carla Combs <azure@manicsquirrel.com>
2022-04-12 20:52:29 -0500

    fix env

--- src/TransCare.Web/ClientApp/src/environments/environment.prod.ts
+++ src/TransCare.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+import AuthConfig from '../../auth_config.json'
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain: AuthConfig.domain,
+    clientId: AuthConfig.clientId,
+    redirectUri: window.location.origin
+  }
 };
