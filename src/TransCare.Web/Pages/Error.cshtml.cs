69ca80eff416eb6e6b6d37a73856777a96fc706b
Carla Combs <azure@manicsquirrel.com>
2022-04-16 23:22:17 -0500

    fix build warning

--- src/TransCare.Web/Pages/Error.cshtml.cs
+++ src/TransCare.Web/Pages/Error.cshtml.cs
@@ -14,7 +14,7 @@ namespace TransCare.Web.Pages
             _logger = logger;
         }
 
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
